52bb07526f1e2a93d8ed52eaf618032eaf3306db
yuvipanda <yuvipanda@gmail.com>
2018-05-17 19:30:39 -0700

    Fix setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setuptools.setup(
     name="nbclearafter",
     version='0.1.0',
     url="https://github.com/yuvipanda/nbclearafter",
-    license="3-Clause BSD"
+    license="3-Clause BSD",
     author="Yuvi Panda",
     description="Clear output of all cells after currently executed cell",
     packages=setuptools.find_packages(),
