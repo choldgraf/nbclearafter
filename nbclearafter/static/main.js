e699fe912de89fd218aaf1ee48443d9f3ac694b2
Chris Holdgraf <choldgraf@berkeley.edu>
2018-05-18 18:24:06 -0700

    adding code styling etc for linear cell flow

--- nbclearafter/static/main.js
+++ nbclearafter/static/main.js
@@ -2,23 +2,50 @@ define([
     'base/js/events'
 ], function (events) {
 
+    var initialize = function () {
+        // add our extension's css to the page
+        $('<link/>')
+            .attr({
+                rel: 'stylesheet',
+                type: 'text/css',
+                href: requirejs.toUrl('./nbclearafter.css')
+            })
+            .appendTo('head');
+    };
+
     // Called every time a cell is executed
     function execute_codecell_callback(e, data) {
         var cell = data.cell;
         var all_cells = cell.notebook.get_cells();
-        var start_clearing = false;
+        var ix_start = all_cells.indexOf(cell);
 
         // Loop through all cells until we find the cell that was just executed
         // Clear output of all cells that come after it.
         for (var i = 0; i < all_cells.length; i++) {
             var cur_cell = all_cells[i];
+            // Skip cells we don't care about
             if (cur_cell.cell_type !== "code") {
-                continue
+                continue;
             }
-            if (start_clearing) {
-                cur_cell.clear_output();
-            } else if (cur_cell.cell_id === cell.cell_id) {
-                start_clearing = true;
+            // Earlier cells should be run in order if they're stale
+            if ((i < ix_start) && cur_cell.element[0].classList.contains('stale')) {
+              cur_cell.execute();
+            }
+            // The current cell should now be editable and active
+            cur_cell.metadata.editable = true;
+            cur_cell.element[0].classList.remove('inactive');
+            cur_cell.element[0].classList.remove('stale');
+            // The next cell is stale but inactive
+            if (i === (ix_start + 1)) {
+              cur_cell.clear_output();
+              cur_cell.element[0].classList.add('stale');
+            }
+            // The remaining cells are both stale and inactive
+            if (i > (ix_start + 1)) {
+              // Subsequent cells are now inactive and stale
+              cur_cell.metadata.editable = false;
+              cur_cell.element[0].classList.add('stale');
+              cur_cell.element[0].classList.add('inactive');
             }
         }
     }
